a25899fad2d2a81293d08923b094a727d09d8022
DreaM <o.v.osypchuk@student.khai.edu>
2021-10-21 14:18:10 +0300

    ChatBot 2-nd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,2 +1,13 @@
-package ChatBot;public class ChatBot {
+package ChatBot;
+
+public class ChatBot {
+
+    public static void main(String[] args) {
+        String botName = "Phill Bot";
+        System.out.println("Hello! My name is " + botName);
+
+        int birth_year = 2021;
+        System.out.println("I was created in " + birth_year);
+    }
+
 }
