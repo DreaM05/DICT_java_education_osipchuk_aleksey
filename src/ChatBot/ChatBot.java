01b657332441313f59242b2fb29b0c06f9429993
DreaM <o.v.osypchuk@student.khai.edu>
2021-10-26 20:13:05 +0300

    ChatBot 4-th stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -29,6 +29,15 @@ public class ChatBot {
         int remainder7 = in.nextInt();
         int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
         System.out.println("Your age is " + age + "; that's a good time to start programming!");
+
+        System.out.println("Now I will prove to you that I can count to any number you want.");
+        int x = in.nextInt();
+        int y = 0;
+        while (y <= x) {
+            System.out.println(y + " !");
+            y++;
+        }
+
         in.close();
     }
 
