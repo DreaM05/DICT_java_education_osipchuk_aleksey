07eff7c54b70a91478329bea0b8befed0d0f5c33
DreaM <o.v.osypchuk@student.khai.edu>
2021-10-28 13:05:28 +0300

    ChatBot 5-th stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -16,12 +16,8 @@ public class ChatBot {
         System.out.println("Please, remind me your name.");
         String name = in.nextLine();
         System.out.println("What a great name you have, " + name + "!");
-        //in.close();
 
         //этап №3
-        //Scanner number1 = new Scanner(System.in);
-        //Scanner number2 = new Scanner(System.in);
-        //Scanner number3 = new Scanner(System.in);
         System.out.println("Let me guess your age.");
         System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
         int remainder3 = in.nextInt();
@@ -30,6 +26,7 @@ public class ChatBot {
         int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
         System.out.println("Your age is " + age + "; that's a good time to start programming!");
 
+        //этап №4
         System.out.println("Now I will prove to you that I can count to any number you want.");
         int x = in.nextInt();
         int y = 0;
@@ -38,6 +35,23 @@ public class ChatBot {
             y++;
         }
 
+        //этап №5
+        System.out.println("Let's test your programming knowledge.");
+        System.out.println("Why do we use methods?");
+        System.out.println("1. To repeat a statement multiple times.");
+        System.out.println("2. To decompose a program into several small subroutines.");
+        System.out.println("3. To determine the execution time of a program.");
+        System.out.println("4. To interrupt the execution of a program.");
+        while (true) {
+            int answer = in.nextInt();
+            if (answer == 2.) {
+                System.out.println("Great, you right!");
+                break;
+            } else {
+                System.out.println("Please, try again.");
+            }
+        }
+        System.out.println("Goodbye, have a nice day!");
         in.close();
     }
 
