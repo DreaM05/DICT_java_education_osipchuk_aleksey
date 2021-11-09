47505362b96f681fc5df4112acd4202bbb4dcbab
DreaM <o.v.osypchuk@student.khai.edu>
2021-11-09 18:48:21 +0200

    Hangman - 1-st stage

---
+++ src/Hangman/hangman.java
@@ -0,0 +1,11 @@
+package Hangman;
+
+public class hangman {
+
+    public static void main (String[] args) {
+
+        System.out.print("HANGMAN \n" + "The game will be available soon");
+
+    }
+
+}
--- src/Test.java
+++
@@ -1,7 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("Java education");
-        //new string
-        System.out.println("DICT");
-    }
-}
