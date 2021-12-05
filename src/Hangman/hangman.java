940f6f22fdc73fa9ec07fb8619ef059db9f39a67
DreaM <o.v.osypchuk@student.khai.edu>
2021-12-05 14:19:57 +0200

    Hangman - 4-th stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -7,29 +7,32 @@ public class hangman {
 
     public static void main (String[] args) {
 
-        //Этап №1
         System.out.print("HANGMAN \n" + "The game will be available soon\n");
-
-        //Этап №2
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Guess the word: ");
-        String variant = scanner.nextLine();
-        if (wordsGenerator().equals(variant)) {
-            System.out.println("You survived!");
-        } else {
-            System.out.println("You lost!");
-        }
-    }
 
-    //Этап №3
-    public static String wordsGenerator() {
         List<String> words = new ArrayList<>();
         words.add("python");
         words.add("java");
         words.add("javascript");
         words.add("kotlin");
         int randomIndex = ThreadLocalRandom.current().nextInt(words.size());
-        String word = words.get(randomIndex);
-        return word;
+
+        for (int i = 0; i < words.get(randomIndex).length(); i++){
+            if (i<2){
+                System.out.print(words.get(randomIndex).charAt(i));
+            }
+            else {
+                System.out.print("-");
+            }
+        }
+
+        System.out.print("\nGuess the word: ");
+        String variant = scanner.nextLine();
+
+        if (words.get(randomIndex).equals(variant)) {
+            System.out.println("You survived!");
+        } else {
+            System.out.println("You lost!");
+        }
     }
 }
