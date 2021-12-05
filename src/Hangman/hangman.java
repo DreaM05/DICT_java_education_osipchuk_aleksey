87b0100c88220f2658bbc445f8235da795212265
DreaM <o.v.osypchuk@student.khai.edu>
2021-12-05 15:10:45 +0200

    Hangman - 6-th stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -27,15 +27,34 @@ public class hangman {
             System.out.print("-");
         }
         char[] word = new char[letters];
+        char[] attempts = new char[20];
+        int cycle = 0;
+        int attempt = 0;
+        int attempt_letter = 0;
         for (int i = 0; i < 8; i++) {
             System.out.println("\nEnter letter:");
             letter = in.next().charAt(0);
             for (int o = 0; o < letters; o++) {
                 if (letter == hiddenword.charAt(o)) {
                     word[o] = letter;
+                    attempt_letter = 1;
                 }
             }
             System.out.println(word);
+            for (int l = 0; l < 20; l++) {
+                if (letter == attempts[l]) {
+                    attempt++;
+                }
+            }
+            if (attempt > 0) {
+                System.out.println("No improvements");
+            }
+            else if (attempt_letter == 1){
+                i--;
+            }
+            else {
+                System.out.println("That letter doesn't appear in the word");
+            }
             for (int p = 0; p < letters; p++) {
                 if (word[p] == hiddenword.charAt(p)) {
                     num++;
@@ -46,6 +65,10 @@ public class hangman {
             } else {
                 num = 0;
             }
+            attempts[cycle] = letter;
+            cycle++;
+            attempt = 0;
+            attempt_letter = 0;
         }
         System.out.println("You lost!");
     }
