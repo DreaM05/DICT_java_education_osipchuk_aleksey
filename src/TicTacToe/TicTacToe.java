435059b1c9e2905c5f4edb8e6739af71ffba5f99
DreaM <o.v.osypchuk@student.khai.edu>
2021-12-10 19:55:22 +0200

    Stage 4 - Added a response to player input and a method for starting the game

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -14,11 +14,38 @@ public class TicTacToe {
     public static Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {    }
-    public static void NachaloIgry(){    }
-    public static void VvodIgroka(){}
+
+    public static void NachaloIgry(){
+        for (int ryad = 0; ryad < RYADKOV; ryad++) {
+            for (int stolb = 0; stolb < STOLBIKOV; stolb++) {
+                setka[ryad][stolb] = PUSTAYA;
+            }
+        }
+        aktivnyiIgrok = KRESTIK;
+        VyvodSetki();
+    }
+
+    public static void VvodIgroka(){
+        boolean vvodDejstvitelen = false;
+        do {
+            System.out.println("Игрок '" + aktivnyiIgrok + "', введите ряд (1-3) и столбец (1-3) через пробел: ");
+            int ryad = in.nextInt() - 1;
+            int stolb = in.nextInt() - 1;
+            if (ryad >= 0 && ryad < RYADKOV && stolb >= 0 && stolb < STOLBIKOV && setka[ryad][stolb] == PUSTAYA) {
+                setka[ryad][stolb] = aktivnyiIgrok;
+                vvodDejstvitelen = true;
+            } else {
+                System.out.println("Выбранная позиция (" + (ryad + 1) + "," + (stolb + 1) + ") не может быть использована. Попробуйте снова...");
+            }
+        } while (!vvodDejstvitelen);
+    }
+
     public static void AnalizSetki(){}
+
     public static boolean ProverkaZapolneniyaKletochek(){}
+
     public static String ProverkaPobeditelya(){}
+
     public static void VyvodSetki(){
         for (int ryad = 0; ryad < RYADKOV; ryad++) {
             for (int stolb = 0; stolb < STOLBIKOV; stolb++) {
