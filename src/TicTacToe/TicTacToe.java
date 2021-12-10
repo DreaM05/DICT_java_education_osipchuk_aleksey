34159a3eb42a8bc0ad51ce3299dcdf58d13534d6
DreaM <o.v.osypchuk@student.khai.edu>
2021-12-10 21:01:13 +0200

    Stage 5 - Added grid analysis and checks for the winner and filling in the cells

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -6,10 +6,26 @@ public class TicTacToe {
     public static final int RYADKOV = 3, STOLBIKOV = 3;
     public static String[][] setka = new String[RYADKOV][STOLBIKOV];
     public static int statusIgry;
-    public static final int STATUS_IGRA_PRODOLJAETSYA = 0, STATUS_NICHIYA = 1, STATUS_POBEDA_X = 2, STATUS_POBEDA_O = 3;
+    public static final int STATUS_IGRA_PRODOLJAETSYA = 0, STATUS_NICHYA = 1, STATUS_POBEDA_X = 3, STATUS_POBEDA_O = 4;
     public static Scanner in = new Scanner(System.in);
 
-    public static void main(String[] args) {    }
+    public static void main(String[] args) {
+        NachaloIgry();
+        do {
+            VvodIgroka();
+            AnalizSetki();
+            VyvodSetki();
+            if (statusIgry == STATUS_POBEDA_X) {
+                System.out.println("'X' побеждает!");
+            } else if (statusIgry == STATUS_POBEDA_O) {
+                System.out.println("'O' побеждает!");
+            } else if (statusIgry == STATUS_NICHYA) {
+                System.out.println("Игра закончилась в ничью!");
+            }
+            aktivnyiIgrok = (aktivnyiIgrok == KRESTIK?NOLIK:KRESTIK);
+        }
+        while (statusIgry == STATUS_IGRA_PRODOLJAETSYA);
+    }
 
     public static void NachaloIgry(){
         for (int ryad = 0; ryad < RYADKOV; ryad++) {
@@ -36,17 +52,72 @@ public class TicTacToe {
         } while (!vvodDejstvitelen);
     }
 
-    public static void AnalizSetki(){}
+    public static void AnalizSetki(){
+        String pobeditel = ProverkaPobeditelya();
+        if (pobeditel.equals(KRESTIK)) {
+            statusIgry = STATUS_POBEDA_X;
+        } else if (pobeditel.equals(NOLIK)) {
+            statusIgry = STATUS_POBEDA_O;
+        } else if (ProverkaZapolneniyaKletochek()) {
+            statusIgry = STATUS_NICHYA;
+        } else {
+            statusIgry = STATUS_IGRA_PRODOLJAETSYA;
+        }
+    }
 
-    public static boolean ProverkaZapolneniyaKletochek(){}
+    public static boolean ProverkaZapolneniyaKletochek(){
+        for (int ryad = 0; ryad < RYADKOV; ryad++) {
+            for (int stolb = 0; stolb < STOLBIKOV; stolb++) {
+                if (setka[ryad][stolb] == PUSTAYA) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
-    public static String ProverkaPobeditelya(){}
+    public static String ProverkaPobeditelya(){
+        int odinakovye;
+        // 3 в ряд
+        for (int ryad = 0; ryad < RYADKOV; ryad++) {
+            odinakovye = 0;
+            for (int stolb = 0; stolb < STOLBIKOV; stolb++) {
+                if (setka[ryad][0] != PUSTAYA && setka[ryad][0] == setka[ryad][stolb]) {
+                    odinakovye++;
+                }
+            }
+            if (odinakovye == 3) {
+                return setka[ryad][0];
+            }
+        }
+        // 3 в столбец
+        for (int stolb = 0; stolb < RYADKOV; stolb++) {
+            odinakovye = 0;
+            for (int ryad = 0; ryad < STOLBIKOV; ryad++) {
+                if (setka[0][stolb] != PUSTAYA && setka[0][stolb] == setka[ryad][stolb]) {
+                    odinakovye++;
+                }
+            }
+            if (odinakovye == 3) {
+                return setka[0][stolb];
+            }
+        }
+        // 3 по диагонали справа налево
+        if (setka[0][0] != PUSTAYA && setka[0][0] == setka[1][1] && setka[0][0] == setka[2][2]) {
+            return setka[0][0];
+        }
+        // 3 по диагонали слева направа
+        if (setka[0][2] != PUSTAYA && setka[1][1] == setka[0][2] && setka[2][0] == setka[2][0]) {
+            return setka[0][2];
+        }
+        return PUSTAYA;
+    }
 
     public static void VyvodSetki(){
         for (int ryad = 0; ryad < RYADKOV; ryad++) {
             for (int stolb = 0; stolb < STOLBIKOV; stolb++) {
                 System.out.print(setka[ryad][stolb]);
-                if (stolb != STOLBIKOV) {
+                if (stolb != STOLBIKOV - 1) {
                     System.out.print("|");
                 }
             }
