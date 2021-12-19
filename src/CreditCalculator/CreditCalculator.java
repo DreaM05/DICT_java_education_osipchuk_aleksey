7d6ed091cec6c873881b0fe07bf8f1c9c0d2d020
DreaM <o.v.osypchuk@student.khai.edu>
2021-12-19 22:40:07 +0200

    1-st Stage - added methods

---
+++ src/CreditCalculator/CreditCalculator.java
@@ -0,0 +1,17 @@
+package CreditCalculator;
+
+public class CreditCalculator {
+
+    public static void main(String[] args) {    }
+
+    public static void credit() {    }
+
+    public static void diff() {    }
+
+    public static void periodyOplaty() {    }
+
+    public static void osnovnayaSumKredita() {    }
+
+    public static void platejMesyachnii() {    }
+
+}
