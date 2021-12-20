6bec2f87250395b457d4e4e6f8c0cbd8fc2ebba2
DreaM <o.v.osypchuk@student.khai.edu>
2021-12-20 04:15:13 +0200

    Stage 2 - added user selection and calculation of necessary operations

--- src/CreditCalculator/CreditCalculator.java
+++ src/CreditCalculator/CreditCalculator.java
@@ -1,13 +1,41 @@
 package CreditCalculator;
 
+
+import java.util.Scanner;
+import static java.lang.Math.ceil;
+
 public class CreditCalculator {
 
     public static void main(String[] args) {
-        System.out.println("Основная сумма кредита: 1000\n"+
-                "Месяц 1: погашено 250\n " +
-                "Месяц 2: погашено 250\n" +
-                "Месяц 3: погашено 250\n " +
-                "Кредит погашен! \n");
+        double principal;
+        double months;
+        double monthly_payments;
+        char parametric;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите вашу сумму: ");
+        principal = in.nextInt();
+        System.out.print("Укажите параметр действия, которые вы хотите сделать (m/n): ");
+        parametric = in.next().charAt(0);
+        if (parametric == 'm') {
+            System.out.print("Ваш ежемесячный платеж:");
+            monthly_payments = in.nextInt();
+            months = principal / monthly_payments;
+            months = ceil(months);
+            System.out.println("Вы должны будете заплатить за" + months + "месяц(а)");
+        } else if (parametric == 'p') {
+            System.out.print("Укажите кол-во месяцев:");
+            months = in.nextInt();
+            monthly_payments = principal / months;
+            monthly_payments = ceil(monthly_payments);
+
+            System.out.print("Ваш ежемесячный платеж = " + monthly_payments);
+            if (principal - (monthly_payments * months) < 0) {
+                monthly_payments = principal - (monthly_payments * (months - 1));
+                System.out.println("Последний платеж составляет = " + monthly_payments);
+            }
+        } else {
+            System.out.println("Ошибка, попробуйте еще раз!");
+        }
     }
 
 }
