b425f73df92b3fe25661521e4fdc3d8209f0b80e
DreaM <o.v.osypchuk@student.khai.edu>
2021-12-20 04:07:05 +0200

    1.1 Stage - another version of the code

--- src/CreditCalculator/CreditCalculator.java
+++ src/CreditCalculator/CreditCalculator.java
@@ -2,16 +2,12 @@ package CreditCalculator;
 
 public class CreditCalculator {
 
-    public static void main(String[] args) {    }
-
-    public static void credit() {    }
-
-    public static void diff() {    }
-
-    public static void periodyOplaty() {    }
-
-    public static void osnovnayaSumKredita() {    }
-
-    public static void platejMesyachnii() {    }
+    public static void main(String[] args) {
+        System.out.println("Основная сумма кредита: 1000\n"+
+                "Месяц 1: погашено 250\n " +
+                "Месяц 2: погашено 250\n" +
+                "Месяц 3: погашено 250\n " +
+                "Кредит погашен! \n");
+    }
 
 }
